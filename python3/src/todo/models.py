ada8470abc37475ff4c4974c49226ab924c8accb
Ryoma Seki <seryouma119@gmail.com>
2019-11-25 11:38:06 +0900

    first commit

--- python3/src/todo/models.py
+++ python3/src/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# from __future__ import unicode_literals
 
 # Create your models here.
+class Todo(models.Model):
+    todo_id = models.AutoField(primary_key=True, max_length=10)
+    title = models.CharField(max_length=50)
+    remind_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- python3/src/todo/views.py
+++ python3/src/todo/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    # return render(request, "music_app/index.html")
-    return HttpResponse("HelloWorld!")
+    all_todo_items = Todo.objects.all()
+    return render(request, "todo/index.html", {'all_todo' : all_todo_items})
+
+
+
+def add(request):
+    todo = request.POST['title']
+    time = request.POST['remind_at']
+    new_todo = Todo( title = todo, remind_at = time )
+    new_todo.save()
+    return redirect("/")
+
+def delete(request, idr):
+    instance = Todo.objects.get(todo_id=idr)
+    instance.delete()
+    return redirect("/")
+
+def edit(request,idr):
+    context = {
+        "todo": Todo.objects.get(todo_id=idr),
+    }
+    return render(request, "todo/edit.html", context)
+
+def edit_process(request,idr):
+    a_title = request.POST["title"]
+    a_remind_at = request.POST["remind_at"]
+    a_created_at = request.POST["created_at"]
+    id = request.POST["whatever"]
+    print(request.POST)
+    ####### need to update ########
+    editing=Todo.objects.get(todo_id=id)
+    editing.title = a_title
+    editing.remind_at = a_remind_at
+    # editing.created_at = a_created_at
+    editing.save()
+    print(editing.title)
+    return redirect("/")
